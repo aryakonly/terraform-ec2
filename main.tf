62153cf4a2f7849be8231a882de4e0dbe8b07ef7
Aryakadam <kadam.arya9058@gmail.com>
2026-02-27 10:34:04 +0530

    security group change

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "Ec2Instance" {
     ami           = "ami-051a31ab2f4d498f5"
     instance_type = "t3.micro"
     key_name = "mumbai-key"
-    security_groups = "my-sg"
+    security_groups = ["my-sg"]
 
     tags = {
       Name = each.key
