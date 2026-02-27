f5b83390f5274bf8f891e80cea2da2b41bb37df4
Aryakadam <kadam.arya9058@gmail.com>
2026-02-27 10:40:16 +0530

    using count

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@ provider "aws" {
 }
 
 resource "aws_instance" "Ec2Instance" {
-  for_each = toset(["instance1", "instance2", "instance3"])
+  #for_each = toset(["instance1", "instance2", "instance3"])
     ami           = "ami-051a31ab2f4d498f5"
     instance_type = "t3.micro"
     key_name = "mumbai-key"
     security_groups = ["my-sg"]
-
+    count = 3
     tags = {
-      Name = each.key
+      Name = "Ec2Instance-${count.index}"
     }
 }
