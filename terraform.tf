7211110bdcfb40b0336764cae2fc2b8747f0f79a
Aryakadam <kadam.arya9058@gmail.com>
2026-03-02 14:56:00 +0530

    key pair creation added

--- terraform.tf
+++ terraform.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "Ec2Instance" {
   #for_each = toset(["instance1", "instance2", "instance3"])
     ami           = "ami-051a31ab2f4d498f5"
     instance_type = "t3.micro"
-    key_name = "mumbai-key"
+    # key_name = "mumbai-key"
     vpc_security_group_ids = [ aws_security_group.my-sg-1.id ]
     # count = 3
     # tags = {
@@ -44,4 +44,9 @@ resource "aws_security_group" "my-sg-1" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_key_pair" "my-mumbai-key" {
+  key_name   = "my-mumbai-key"
+  public_key = file("~/.ssh/mumbai-key.pub")
 }
